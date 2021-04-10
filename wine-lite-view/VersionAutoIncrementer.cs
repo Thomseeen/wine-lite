49bdccde9c76cc75b1340291c8daf2bf1bee4bbb
thomseeen <thwa1@web.de>
2021-04-10 17:45:50 +0200

    one exe/dll comp

--- wine-lite-view/VersionAutoIncrementer.cs
+++ wine-lite-view/VersionAutoIncrementer.cs
@@ -1,6 +1,6 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.1.918")]
-[assembly: AssemblyFileVersion("0.1.1.918")]
+[assembly: AssemblyVersion("0.1.1.933")]
+[assembly: AssemblyFileVersion("0.1.1.933")]
 
